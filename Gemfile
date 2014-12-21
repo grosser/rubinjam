3e8390eeaae6ba98b2566c9c69f9d85de2b8cff7
grosser <michael@grosser.it>
2014-12-20 19:34:50 -0800

    stream response to avoid heroku timeout

--- Gemfile
+++ Gemfile
@@ -8,5 +8,6 @@ gem "byebug", :platform => [:ruby_20, :ruby_21]
 
 # server
 gem "sinatra"
+gem "sinatra-contrib"
 gem "json"
 gem "thin"
--- server/app.rb
+++ server/app.rb
@@ -1,9 +1,35 @@
 require 'sinatra'
+require 'sinatra/streaming'
 require 'open-uri'
 require 'tmpdir'
 require 'rubinjam'
 
+# increase the timeout by fake streaming the response
+# test with `time curl https://rubinjam.herokuapp.com/pack/s3_meta_sync -vvv > s3-meta-sync`
+def stream_result(out, work)
+  buffer = Rubinjam::HEADER.dup
+  loop do
+    break if buffer.empty?
+    20.times do # check often -> don't hang for long
+      if !work.alive?
+        out.print buffer # we are done, write the remaining buffer
+        out.print work.value.sub(Rubinjam::HEADER, '')
+        return
+      end
+      sleep 1
+    end
+    out.print buffer.slice!(0, 1)
+    out.flush
+  end
+
+  # we already sent the '200 OK' ... tell the user something useful ...
+  out.print "\nraise 'code generation took too long'"
+end
+
 get "/pack/:gem/?:version?" do
   content_type 'text/plain'
-  Rubinjam.pack_gem(params[:gem], params[:version]).last
+  work = Thread.new { Rubinjam.pack_gem(params[:gem], params[:version]).last }
+  stream do |out|
+    stream_result(out, work)
+  end
 end
